91d72942e91c39d62989aaee677e8e3c9e858155
panda-satyabrata <satyabrat.panda97@gmail.com>
2020-09-19 20:25:15 +0530

    Updates

---
+++ precache-manifest.17cadaf8d1985182464072ebc4646280.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db732a7c9f35198ea420a7f6b0c4225a",
+    "url": "/todo-list/index.html"
+  },
+  {
+    "revision": "815d283f30e8ff081a55",
+    "url": "/todo-list/static/css/main.82aaa009.chunk.css"
+  },
+  {
+    "revision": "d20176e84a2761b0c1d9",
+    "url": "/todo-list/static/js/2.b28a15d9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/todo-list/static/js/2.b28a15d9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "815d283f30e8ff081a55",
+    "url": "/todo-list/static/js/main.89fc1401.chunk.js"
+  },
+  {
+    "revision": "a557b42a7f266985029e",
+    "url": "/todo-list/static/js/runtime-main.9c8ee466.js"
+  }
+]);
